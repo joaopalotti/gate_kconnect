251a8436b7c67a013bda7f2388da64b1ae907b5b
joaopalotti <joaopalotti@github.com>
2015-08-10 17:38:47 +0200

    First version of processing tools

---
+++ go.sh
@@ -0,0 +1,3 @@
+javac BatchProcessApp.java
+java -cp $CLASSPATH:/data/palotti/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessApp -g /data/palotti/gate_khresmoi/khresmoi-app/khresmoi-applications/application-noedit.xgapp  -a Disease -a Drug -a Investigation -a Anatomy -i inputs/ -o outputs/
+
---
+++ go_logs.sh
@@ -0,0 +1,3 @@
+javac BatchProcessLogs.java
+java -cp $CLASSPATH:/data/palotti/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessLogs -g /data/palotti/gate_khresmoi/khresmoi-app/khresmoi-applications/application-noedit.xgapp  -a Disease -a Drug -a Investigation -a Anatomy -i inputs/ -o outputs/
+
