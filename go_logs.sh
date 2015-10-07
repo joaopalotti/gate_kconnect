87012a1bfb6e0a253c88a5a3bc468d5d3afd3583
joaopalotti <joaopalotti@gmail.com>
2015-10-07 12:11:38 +0200

    Updated input settings

--- go_logs.sh
+++ go_logs.sh
@@ -1,3 +1,10 @@
-javac BatchProcessLogs.java
-java -cp $CLASSPATH:/data/palotti/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessLogs -g /data/palotti/gate_khresmoi/khresmoi-app/khresmoi-applications/application-noedit.xgapp  -a Disease -a Drug -a Investigation -a Anatomy -i inputs/ -o outputs/
+#javac -cp $CLASSPATH:jars/*:/home/palotti/Dropbox/kconnect/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessLogs.java
+
+#java -cp $CLASSPATH:jars/*:/home/palotti/Dropbox/kconnect/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessLogs -g /home/palotti/Dropbox/kconnect/gate_khresmoi/khresmoi-app/khresmoi-applications/application-noedit.xgapp  -a Disease -a Drug -a Investigation -a Anatomy -i /home/palotti/Dropbox/kconnect/allan/allan_allclicks_fixed.csv -o outputs/
+
+java -cp $CLASSPATH:jars/*:/home/palotti/Dropbox/kconnect/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessLogs -g  /home/palotti/Dropbox/kconnect/gate_khresmoi/khresmoi-app/khresmoi-applications/application-noedit.xgapp  -a Disease -a Drug -a Investigation -a Anatomy -i /home/palotti/Dropbox/kconnect/trip/clickstream_allusers_sinceJan2014.csv  -o output.csv
+
+#java -cp $CLASSPATH:jars/*:/home/palotti/Dropbox/kconnect/gate_khresmoi/GATE_Developer_8.1/lib/* BatchProcessLogs -g  /home/palotti/Dropbox/kconnect/gate_khresmoi/khresmoi-app/khresmoi-applications/application-noedit.xgapp  -a Disease -a Drug -a Investigation -a Anatomy -i /home/palotti/Dropbox/kconnect/trip/h10 -o output.csv
+
+# Other -a not used: DrugBank
 
